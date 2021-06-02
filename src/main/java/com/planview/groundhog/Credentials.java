691a73a48ca97bd511e2be5735f8ccdb798779be
Nik Antonelli <nikantonelli@users.noreply.github.com>
2021-06-02 17:23:20 +0100

    Add a bit of xlsx handling

---
+++ src/main/java/com/planview/groundhog/Credentials.java
@@ -0,0 +1,10 @@
+package com.planview.groundhog;
+
+//All fieldnames must be lowercase alphabetical
+public class Credentials {
+    public String url;  //Must be first in this object.
+    public String username;
+    public String password;
+    public String apikey;
+    public Double cyclelength;  //Excel numeric fields are doubles
+}
--- src/main/java/com/planview/groundhog/GroundHog.java
+++ src/main/java/com/planview/groundhog/GroundHog.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Iterator;
 
-import org.apache.bcel.generic.FNEG;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
@@ -15,17 +17,72 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class GroundHog {
+
+    Integer refreshPeriod = 14; // Number of days to run for by default
+    Credentials creds = new Credentials();
+    String xlsxfn = "";
+    FileInputStream xlsxfis = null;
+    XSSFWorkbook wb = null;
+    static Boolean useCron = false;
+
+    /**
+     * One line sheet that contains the credentials to access the Leankit Server
+     * Must contain columns "url", "username", "password" and "apiKey", but not
+     * necessarily have data in all of them - see getConfig()
+     */
+    XSSFSheet configSht = null;
+
     public static void main(String[] args) {
 
+        GroundHog hog = new GroundHog();
+
+        hog.getCommandLine(args);
+        hog.parseXlsx();
+        hog.getConfig();
+        Integer day = 0;
+
+        // Check to see if there is command line option to loop or use cron
+        if (!useCron) {
+            while (true) {
+                try {
+                    Calendar now = Calendar.getInstance();
+                    Calendar then = Calendar.getInstance();
+                    then.add(Calendar.DATE, 1);
+                    then.set(Calendar.HOUR_OF_DAY, 3); // Set to three in the morning
+                    then.set(Calendar.MINUTE, 0);
+                    then.set(Calendar.SECOND, 0);
+                    Long timeDiff = then.getTimeInMillis() - now.getTimeInMillis();
+                    // Do todays activity and then sleep
+                    hog.activity(day++);
+                    System.out.println("Sleeping until: " + then.getTime());
+                    Thread.sleep(timeDiff);
+                } catch (InterruptedException e) {
+                    System.out.println("Early wake from daily timer");
+                    System.exit(1);
+                }
+            }
+        } else {
+            // Create a local file to hold the day
+
+            // Delete file after the time period has expired
+        }
+    }
+
+    public void getCommandLine(String[] args) {
         Options opts = new Options();
         Option xlsxname = new Option("f", "filename", true, "source XLSX spreadsheet");
         xlsxname.setRequired(true);
         opts.addOption(xlsxname);
+        Option cronIt = new Option("c", "cron", false, "Program is called by cron job");
+        cronIt.setRequired(false);
+        opts.addOption(cronIt);
 
         CommandLineParser p = new DefaultParser();
         HelpFormatter hf = new HelpFormatter();
@@ -38,42 +95,49 @@ public class GroundHog {
             System.exit(1);
         }
 
-        String fn = cl.getOptionValue("filename");
+        xlsxfn = cl.getOptionValue("filename");
+        useCron = cl.hasOption("cron");
+    }
 
+    /**
+     * Check if the XLSX file provided has the correct sheets and we can parse the
+     * details we need
+     */
+    public void parseXlsx() {
         // Check we can open the file
-        FileInputStream fis = null;
         try {
-            fis = new FileInputStream(new File(fn));
+            xlsxfis = new FileInputStream(new File(xlsxfn));
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
-            hf.printHelp(" ", opts);
+
             System.exit(1);
 
         }
-
-        XSSFWorkbook wb = null;
         try {
-            wb = new XSSFWorkbook(fis);
+            wb = new XSSFWorkbook(xlsxfis);
         } catch (IOException e) {
             System.out.println(e.getMessage());
-            hf.printHelp(" ", opts);
             System.exit(1);
         }
-       
-        //These two come first in the file. 
-        XSSFSheet configSht = wb.getSheet("Config");
+
+        // These two should come first in the file and must be present
+
+        configSht = wb.getSheet("Config");
+        // This is a common sheet listing all the progression changes for all boards
         XSSFSheet changesSht = wb.getSheet("Changes");
 
         Integer shtCount = wb.getNumberOfSheets();
-        if (shtCount < 3) {
-            System.out.println("Did not detect enough sheets in the spreadsheet");
+        if ((shtCount < 3) || (configSht == null) || (changesSht == null)) {
+            System.out.println(
+                    "Did not detect correct sheets in the spreadsheet: \"Config\",\"Changes\" and one, or more, board(s)");
             System.exit(1);
         }
 
+        // Grab the rest of the sheets that describe boards
         ArrayList<XSSFSheet> shtArray = new ArrayList<XSSFSheet>();
         Iterator<Sheet> si = wb.iterator();
-        
-        //Then all the level info comes next
+
+        // Then all the level info comes next
         while (si.hasNext()) {
             XSSFSheet n = (XSSFSheet) si.next();
             if (!((n.getSheetName().equals("Config")) || (n.getSheetName().equals("Changes")))) {
@@ -81,11 +145,114 @@ public class GroundHog {
             }
 
         }
+    }
 
+    /**
+     * To get access to the Leankit server, we need either a url/apiKey pair or a
+     * username/password pair We will use the apiKey in preference if it is present.
+     * Fallback is Base64 encoding of username/password If neither, then barf!
+     * 
+     * @return None
+     */
+    private void getConfig() {
+        // Make the contents of the file lower case before comparing with these.
+        Field[] p = (new Credentials()).getClass().getDeclaredFields();
 
+        ArrayList<String> cols = new ArrayList<String>();
+        for (int i = 0; i < p.length; i++) {
+            p[i].setAccessible(true); // Set this up for later
+            cols.add(p[i].getName().toLowerCase());
+        }
+        HashMap<String, Object> hm = new HashMap<>();
 
+        // Assume that the titles are the first row
+        Iterator<Row> ri = configSht.iterator();
+        Row hdr = ri.next();
+        Iterator<Cell> cptr = hdr.cellIterator();
+
+        while (cptr.hasNext()) {
+            Cell cell = cptr.next();
+            Integer idx = cell.getColumnIndex();
+            String cellName = cell.getStringCellValue().trim().toLowerCase();
+            if (cols.contains(cellName)) {
+                hm.put(cellName, idx); // Store the column index of the field
+            }
+        }
+
+        if (hm.size() != cols.size()) {
+            System.out.println("Did not detect correct columns on Config sheet: " + cols.toString());
+            System.exit(1);
+        }
+
+        // Now we know which columns contain the data, scan down the sheet looking for a
+        // row with data in the 'url' cell
+        while (ri.hasNext()) {
+            Row drRow = ri.next();
+            String cv = drRow.getCell(Integer.parseInt(hm.get(cols.get(0)).toString())).getStringCellValue();
+            if (cv != null) {
+
+                for (int i = 0; i < cols.size(); i++) {
+
+                    try {
+                        String idx = cols.get(i);
+                        Object obj = hm.get(idx);
+                        String val = obj.toString();
+                        Cell cell = drRow.getCell(Integer.parseInt(val));
+
+                        if (cell != null) {
+                            switch (cell.getCellType()) {
+                                case STRING:
+                                    p[i].set(creds,
+                                            (cell != null ? drRow.getCell(Integer.parseInt(val)).getStringCellValue()
+                                                    : ""));
+                                    break;
+                                case NUMERIC:
+                                    p[i].set(creds,
+                                            (cell != null ? drRow.getCell(Integer.parseInt(val)).getNumericCellValue()
+                                                    : ""));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        else {
+                            p[i].set( creds, (p[i].getType().equals(String.class))? "": 0.0);
+                        }
+
+                    } catch (IllegalArgumentException | IllegalAccessException e) {
+                        System.out.println(e.getMessage());
+                        System.exit(1);
+                    }
+
+                }
+
+                break; // Exit out of while loop as we are only interested in the first one we find
+            }
+        }
+
+        // Creds are now found and set. If not, you're buggered.
+        // We can opt to use username/password or apikey.
+
+        if ((creds.cyclelength != null) && (creds.cyclelength != 0.0)) {
+            refreshPeriod = creds.cyclelength.intValue();
+        }
+        return;
+    }
+
+    private void activity(Integer day) {
+        // Check if beyond the time limit, if not check for things to do
+        if (day > refreshPeriod) {
+            day = 0;
+            reset();
+        }
         
-        System.out.println(fn);
+    }
 
+    private void reset() {
+        // Clear out all
+    }
+
+    private Integer getRefresh() {
+        return refreshPeriod;
     }
 }
